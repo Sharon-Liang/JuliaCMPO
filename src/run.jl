628560d20c1d8ae2573508bce8ba38de770c2f04
Sharon-Liang <liangshuang228@gmail.com>
2021-02-24 16:39:18 +0800

    minor modification
    use optimized psi as the next initial psi

--- src/run.jl
+++ src/run.jl
@@ -3,19 +3,19 @@ using Optim
 using DelimitedFiles
 
 χ = 10
-ψ = init_cmps(χ)
-arr = toarray(ψ)
-
 W = TFIsing(1.,1.)
 
-len = 10
-β = [i for i in range(20,30,length = len)]
+len = 100
+β = [i for i in range(0.1,10,length = len)]
 
-open("./data/lowT.txt","w") do io
+open("./data/chi10.txt","w") do io
+    ψ = init_cmps(χ)
+    arr = toarray(ψ)
     for b in β
         of(x::Array{Float64, 3}) = OptimFreeEnergy(x::Array{Float64, 3}, W, b)
         of!(gx::Array{Float64, 3}, x::Array{Float64,3}) = OptimFreeEnergy!(gx::Array{Float64, 3}, x::Array{Float64,3}, W, b)
         op = optimize(of, of!, arr, LBFGS())
         writedlm(io,[b minimum(op)])
+        arr = op.minimizer
     end
 end
