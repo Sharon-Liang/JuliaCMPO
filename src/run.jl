59873f5ff19754d72144fd50d4de16e00dfaf6e0
Sharon-Liang <liangshuang228@gmail.com>
2021-02-24 22:43:26 +0800

    new F-F_exact fig

--- src/run.jl
+++ src/run.jl
@@ -5,13 +5,13 @@ using DelimitedFiles
 χ = 10
 W = TFIsing(1.,1.)
 
-len = 100
-β = [i for i in range(0.1,10,length = len)]
+#len = 100
+#β = [i for i in range(0.1,10,length = len)]
 
-#len = 50
-#β = [10^i for i in range(-2,5,length = len)]
+len = 50
+β = [10^i for i in range(-2,5,length = len)]
 
-open("./data/chi10.txt","w") do io
+open("./data/chi10-log.txt","w") do io
     ψ = init_cmps(χ)
     arr = toarray(ψ)
     for b in β
