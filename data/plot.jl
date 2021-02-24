39eae9eda7912571a7d75555fca6598341473273
Sharon-Liang <liangshuang228@gmail.com>
2021-02-24 23:07:26 +0800

    low temperature plot

--- data/plot.jl
+++ data/plot.jl
@@ -1,14 +1,15 @@
 using PyPlot
 using DelimitedFiles
 
-data = readdlm("./data/chi10.txt")
-exact = readdlm("./data/ExactIsing.txt")
+data = readdlm("./data/chi10-log.txt")
+exact = readdlm("./data/ExactIsing-log.txt")
 
 title(" TFIM: J = Gamma = 1, chi = 10")
-xlim([0,10])
+xlim([10^-2,10^5])
+ylim([0.,4*10^-6])
 xlabel("beta")
 ylabel("F - F_exact")
-plot(data[:,1], data[:,2] - exact[:,2])
+semilogx(data[:,1], data[:,2] - exact[:,2])
 
-savefig("./data/F_difference.pdf")
+savefig("./data/F_difference-log.pdf")
 PyPlot.display_figs()
