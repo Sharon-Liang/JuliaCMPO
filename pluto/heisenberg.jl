215caf06bbe8b434e7b7f3299d3278e37893c1cd
sliang <sliang@iphy.ac.cn>
2021-08-17 11:54:17 +0800

    update

---
+++ pluto/heisenberg.jl
@@ -0,0 +1,109 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bb98f3ec-fe48-11eb-0fa0-5d8f4b23b417
+begin
+	using Pkg
+	Pkg.activate("..")
+	using cMPO
+	using DelimitedFiles, HDF5, JLD
+	using Plots
+	using Printf
+	using LsqFit
+	using LinearAlgebra
+	using Arpack
+	"load packages"
+end
+
+# ╔═╡ bad9c632-c7d7-4461-b4b6-c489a3b91319
+beta = [i for i in range(1,100,step=0.1)]
+
+# ╔═╡ 9e0bcc39-b7c1-417c-94d2-9bd8935f0b08
+begin
+	data =  load("../data/heisenberg.jld")
+	"../data/heisenberg.jld" 
+end
+
+# ╔═╡ 1a1509c0-50ef-4e61-8f4b-0b83b32e6e9d
+begin
+	z = make_operator(pauli(:z),8)
+	x = make_operator(pauli(:x),8)
+	"x, z operator"
+end
+
+# ╔═╡ b1c6f6b4-a907-4cdf-808c-39ba0ad89e3b
+function nmr_relaxiation(η::Float64)
+	d = data
+	w = HeisenbergModel()
+	len = length(beta)
+	res = zeros(len)
+	for i = 1:len
+		β = beta[i]; key = string(β)
+		ψ = tocmps(d[key][2])
+		res[i] = structure_factor(0,z,z,ψ,w,β,η=η)
+	end
+	return res
+end
+
+# ╔═╡ c091b721-c654-4d47-b448-4890762856a3
+function nmr_relaxiation()
+	d = data
+	w = HeisenbergModel()
+	len = length(beta)
+	res = zeros(len)
+	for i = 1:len
+		β = beta[i]; key = string(β)
+		ψ = tocmps(d[key][2])
+		res[i] = π/4*β*correlation_2time(β/2,z,z,ψ,w,β)
+	end
+	return res
+end
+
+# ╔═╡ cb5fbdf3-0a91-4e55-8833-b5d116c1634d
+function plot_nmr(η::Float64)
+	datalab = @sprintf "Heisenberg model: η=%.3f" η
+	t = nmr_relaxiation(η)
+	plot(1 ./ beta, t, line=(:dash), marker=(:circle,3),label=datalab)
+	plot!(xlabel="T", ylabel = "S(0)")
+end
+
+# ╔═╡ 0249eb44-011a-440a-827b-789df5fe33e1
+function plot_nmr()
+	datalab = @sprintf "Heisenberg model: β/2 average"
+	t = nmr_relaxiation()
+	plot(1 ./ beta, t, line=(:dash), marker=(:circle,3),label=datalab)
+	plot!(xlabel="T", ylabel = "S(0)")
+end
+
+# ╔═╡ cfae3287-603f-4638-85f7-3d7ef65b7f9b
+plot_nmr(0.05)
+
+# ╔═╡ 51cd4196-2aca-4dee-9b0d-da30ebc76305
+plot_nmr(0.1)
+
+# ╔═╡ cab4489c-5ba1-470f-a4b6-efeff6611070
+plot_nmr(0.005)
+
+# ╔═╡ 9d5f161f-eae2-4770-8647-6b61bd208b2a
+plot_nmr()
+
+# ╔═╡ 58016c22-5c97-4a1f-bff2-173cbaaf00cd
+pwd()
+
+# ╔═╡ Cell order:
+# ╟─bad9c632-c7d7-4461-b4b6-c489a3b91319
+# ╟─9e0bcc39-b7c1-417c-94d2-9bd8935f0b08
+# ╠═b1c6f6b4-a907-4cdf-808c-39ba0ad89e3b
+# ╠═c091b721-c654-4d47-b448-4890762856a3
+# ╠═cb5fbdf3-0a91-4e55-8833-b5d116c1634d
+# ╠═0249eb44-011a-440a-827b-789df5fe33e1
+# ╠═cfae3287-603f-4638-85f7-3d7ef65b7f9b
+# ╠═51cd4196-2aca-4dee-9b0d-da30ebc76305
+# ╠═cab4489c-5ba1-470f-a4b6-efeff6611070
+# ╠═9d5f161f-eae2-4770-8647-6b61bd208b2a
+# ╟─1a1509c0-50ef-4e61-8f4b-0b83b32e6e9d
+# ╟─bb98f3ec-fe48-11eb-0fa0-5d8f4b23b417
+# ╠═58016c22-5c97-4a1f-bff2-173cbaaf00cd
--- pluto/nmr_relaxiation.jl
+++ pluto/nmr_relaxiation.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.8
+# v0.15.1
 
 using Markdown
 using InteractiveUtils
@@ -71,7 +71,7 @@ begin
 end
 
 # ╔═╡ dfe621e5-a860-47e0-b670-2fd4f57eae68
-function nmr_relaxiation(g::Real;η::Float64=0.05)
+function nmr_relaxiation(g::Real,η::Float64=0.05)
 	i = findall(x-> x==g, gamma)[1]
 	d = data[i]
 	w = TFIsing(1., g)
@@ -85,10 +85,25 @@ function nmr_relaxiation(g::Real;η::Float64=0.05)
 	return res
 end
 
+# ╔═╡ 4dac8cf2-8e9d-4aa5-9061-1228332a08b9
+function nmr_relaxiation(g::Real)
+	i = findall(x-> x==g, gamma)[1]
+	d = data[i]
+	w = TFIsing(1., g)
+	len = length(beta)
+	res = zeros(len)
+	for i = 1:len
+		β = beta[i]; key = string(β)
+		ψ = tocmps(d[key][2])
+		res[i] = π/4*β*correlation_2time(β/2,z,z,ψ,w,β)
+	end
+	return res
+end
+
 # ╔═╡ 5f036037-0e4a-42a8-be10-2e467ca1015d
-function plot_nmr(g::Real; η::Float64=0.05)
+function plot_nmr(g::Real, η::Float64)
 	datalab = @sprintf "Γ/J=%.1f,η=%.3f" g η
-	t = nmr_relaxiation(g, η=η)
+	t = nmr_relaxiation(g, η)
 	if g==1
 		xdata = beta
 		ydata = t
@@ -103,49 +118,76 @@ function plot_nmr(g::Real; η::Float64=0.05)
 	plot!(1 ./ beta, f(beta), lw = 2, label =fname, ylim = ylimit)
 end
 
+# ╔═╡ e72e5cc9-0ad4-42d9-be50-979134a7055a
+function plot_nmr(g::Real)
+	datalab = @sprintf "Γ/J=%.1f,β/2 average" g
+	t = nmr_relaxiation(g)
+	xdata = beta
+	ydata = t
+	ylimit = (minimum(t)*0.8, maximum(t)*1.1)
+	plot(1 ./ beta, t, line=(:dash), marker=(:circle,3),label=datalab)
+	plot!(xlabel="T", ylabel = "S(0)")
+	f, fname = fitnmr(g, xdata, ydata)
+	plot!(1 ./ beta, f(beta), lw = 2, label =fname, ylim = ylimit)
+end
+
 # ╔═╡ 8c6de546-fc37-411c-8ee8-c8b331056142
-plot_nmr(1.0)
+plot_nmr(1.0, 0.05)
 
 # ╔═╡ 3645514b-8f48-4607-ad2c-1ed36b15b741
-plot_nmr(1.0, η = 0.1)
+plot_nmr(1.0, 0.1)
 
 # ╔═╡ 69f9a51a-518a-46bc-9cd9-c1be6908f433
-plot_nmr(1.0, η = 0.005)
+plot_nmr(1.0, 0.005)
 
 # ╔═╡ 797747bb-6e76-47a1-8a39-f2ad99158f32
-plot_nmr(0.5)
+plot_nmr(0.5,0.05)
 
 # ╔═╡ 85b7398c-754d-43ae-93f8-2847c78581e5
-plot_nmr(0.5, η = 0.1)
+plot_nmr(0.5, 0.1)
 
 # ╔═╡ 1bc84f68-08c9-47fa-8e3a-9dc5d7d31d47
-plot_nmr(0.5, η = 0.005)
+plot_nmr(0.5, 0.005)
 
 # ╔═╡ 1729c59d-3c4e-450d-8c68-d742372e0063
-plot_nmr(2., η = 0.05)
+plot_nmr(2., 0.05)
 
 # ╔═╡ 734c4847-1f03-478a-9346-90ef3e0a376b
-plot_nmr(2., η = 0.1)
+plot_nmr(2.,0.1)
 
 # ╔═╡ 3e95bb33-0c55-45b2-8a16-78a5f1f0fbcc
-plot_nmr(2., η = 0.005)
+plot_nmr(2., 0.005)
+
+# ╔═╡ f3b5afce-f8b5-4177-b6cb-86b3e9140972
+plot_nmr(2.)
+
+# ╔═╡ 28fb60ad-00c6-4423-8102-fc76c0e8711f
+plot_nmr(1.)
+
+# ╔═╡ 812d5668-44bc-4011-8ed5-7e907dc4dcde
+plot_nmr(0.5)
 
 # ╔═╡ Cell order:
 # ╟─838f933c-db31-4887-b206-562bd060746f
 # ╟─f4fda113-17ce-4312-93ff-fb361815b970
 # ╟─3f990c4b-97da-4336-8e34-1bd496162328
-# ╟─8bf42a6a-51eb-42b4-b9aa-e6f8a5148d2f
-# ╟─dfe621e5-a860-47e0-b670-2fd4f57eae68
-# ╟─9dd8334d-7664-44f9-b052-b3858d913066
-# ╟─5f036037-0e4a-42a8-be10-2e467ca1015d
+# ╠═8bf42a6a-51eb-42b4-b9aa-e6f8a5148d2f
+# ╠═dfe621e5-a860-47e0-b670-2fd4f57eae68
+# ╠═4dac8cf2-8e9d-4aa5-9061-1228332a08b9
+# ╠═9dd8334d-7664-44f9-b052-b3858d913066
+# ╠═5f036037-0e4a-42a8-be10-2e467ca1015d
+# ╟─e72e5cc9-0ad4-42d9-be50-979134a7055a
 # ╟─8c6de546-fc37-411c-8ee8-c8b331056142
 # ╟─3645514b-8f48-4607-ad2c-1ed36b15b741
 # ╟─69f9a51a-518a-46bc-9cd9-c1be6908f433
-# ╟─797747bb-6e76-47a1-8a39-f2ad99158f32
+# ╠═797747bb-6e76-47a1-8a39-f2ad99158f32
 # ╟─85b7398c-754d-43ae-93f8-2847c78581e5
 # ╟─1bc84f68-08c9-47fa-8e3a-9dc5d7d31d47
 # ╟─1729c59d-3c4e-450d-8c68-d742372e0063
 # ╟─734c4847-1f03-478a-9346-90ef3e0a376b
 # ╟─3e95bb33-0c55-45b2-8a16-78a5f1f0fbcc
+# ╠═f3b5afce-f8b5-4177-b6cb-86b3e9140972
+# ╠═28fb60ad-00c6-4423-8102-fc76c0e8711f
+# ╠═812d5668-44bc-4011-8ed5-7e907dc4dcde
 # ╟─05f4d459-5bc7-4dbf-ab13-42041c0cf8e7
 # ╟─9f3511fc-e24d-11eb-130d-15fd034ec045
