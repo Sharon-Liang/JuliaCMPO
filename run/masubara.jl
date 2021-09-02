0d314ab99fcd1f15c089a16d613d9d4c7e1397ff
sliang <sliang@iphy.ac.cn>
2021-09-02 11:25:23 +0800

    update

--- run/masubara.jl
+++ run/masubara.jl
@@ -4,36 +4,40 @@ using DelimitedFiles
 using JLD, HDF5
 using Printf
 
-g = 1.0
-path = @sprintf "./data/g_%.1f.jld" g
-d1 = load(path);
-β = 10.0
-key = string(β)
+z = make_operator(pauli(:z), 8)
 
-w = TFIsing(1.,g)
-ψ = d1[key][2] |> tocmps
-z = make_operator(pauli(:z), ψ)
-Nfreq = 20
+gamma = [0.5, 1.0, 2.0, 4.0, 8.0]
+for g in gamma
+    path = @sprintf "./data/g_%.1f.jld" g
+    d1 = load(path);
+    β = 10.0
+    key = string(β)
 
-path1 = @sprintf "./data/Acmpo.txt"
-path2 = @sprintf "./data/input.txt" # frequency real_part imag_part \n, increasing masubara frenquancies
+    w = TFIsing(1.,g)
+    ψ = d1[key][2] |> tocmps
 
-open(path1, "w") do file
-end
+    Nfreq = 20
 
-open(path2, "w") do file
-end
+    path1 = @sprintf "./data/Acmpo_%.1f.txt" g
+    path2 = @sprintf "/home/sliang/hardy_optim_clean_submission/ifile_%.1f.txt" g # frequency real_part imag_part \n, increasing masubara frenquancies
 
-for i in range(-10,10,step=0.05)
-    open(path1, "a") do file1
-        writedlm(file1,[i spectral_function(i,z,z,ψ,w,β,η=0.001)])
+    open(path1, "w") do file
     end
-end
 
-for i = 1:Nfreq
-    freq = Masubara_freq(i,β) |> imag
-    gn = Masubara_GF(i,z,z,ψ,w,β)
-    open(path2, "a") do file2
-        writedlm(file2,[freq real(gn) imag(gn)])
+    open(path2, "w") do file
+    end
+
+    for i in range(0,10,step=0.05)
+        open(path1, "a") do file1
+            writedlm(file1,[i spectral_function(i,z,z,ψ,w,β,η=0.001)])
+        end
+    end
+
+    for i = 1:Nfreq
+        freq = Masubara_freq(i,β) |> imag
+        gn = Masubara_GF(i,z,z,ψ,w,β)
+        open(path2, "a") do file2
+            writedlm(file2,[freq real(gn) imag(gn)])
+        end
     end
 end
