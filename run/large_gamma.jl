fb0a4ac0c0633fed06064677aa4d9503973b07f3
Sharon-Liang <liangshuang228@gmail.com>
2021-09-06 16:24:07 +0800

    update plot
    correction of exact structure factor calculation

--- run/large_gamma.jl
+++ run/large_gamma.jl
@@ -14,17 +14,17 @@ J = 0.; key =string(J)
 ψ = tocmps(d[key][2])
 w = TFIsing(0.,1.)
 
+ψ0 = init_cmps(2,w)
+z0 = make_operator(pauli(:z),ψ0)
+
+#correlation_2time
 tau = [t for t in range(0,β,length=100)]
 
 gt1 = [correlation_2time(t,z,z,ψ,w,β) for t in tau]
 gt2 = [correlation_2time(0,t,β) for t in tau]
-
 dg = gt1 .- gt2
 
-ψ0 = init_cmps(2,w)
-z0 = make_operator(pauli(:z),ψ0)
 gt3 = [correlation_2time(t,z0,z0,ψ0,w,β) for t in tau]
-
 dg2 = gt3 .- gt2
 
 τ = tau ./ β
@@ -49,12 +49,11 @@ title(@sprintf "β = %i" β)
 PyPlot.savefig("atomic_gf_diff.pdf",bbox_inches="tight")
 PyPlot.display_figs()
 
-#spectral_function
-
+#spectral_density
 omega = [i for i in range(-4,4, length=100)]
-a1 = [spectral_function(0.,x,β) for x in omega]
-a2 = [spectral_function(x,z,z, ψ, w,β) for x in omega]
-a3 = [spectral_function(x,z0,z0, ψ0, w,β) for x in omega]
+a1 = [spectral_density(0.,x,β) for x in omega]
+a2 = [spectral_density(x,z,z, ψ, w,β) for x in omega]
+a3 = [spectral_density(x,z0,z0, ψ0, w,β) for x in omega]
 
 figure()
 plot(omega, a1, linewidth=2, label="theory")
@@ -75,3 +74,27 @@ legend()
 title(@sprintf "β = %i" β)
 PyPlot.savefig("atomic_imag_chi_diff.pdf",bbox_inches="tight")
 
+#structure_factor
+omega = [i for i in range(1.e-5,4, length=100)]
+a1 = [structure_factor(0.,x,β) for x in omega]
+a2 = [structure_factor(x,z,z, ψ, w,β) for x in omega]
+a3 = [structure_factor(x,z0,z0, ψ0, w,β) for x in omega]
+
+figure()
+plot(omega, a1, linewidth=2, label="theory")
+plot(omega, a2, "--o", linewidth = 2, label = "χ=8 cmpo")
+plot(omega, a3, "--*", linewidth = 2, label = "χ=2 cmpo")
+xlabel("ω")
+ylabel("S(ω)")
+legend()
+title(@sprintf "β = %i" β)
+PyPlot.savefig("atomic_struc.pdf",bbox_inches="tight")
+
+figure()
+plot(omega, abs.(a2 .- a1), linewidth = 2, label = "χ=8 cmpo")
+plot(omega, abs.(a3 .- a1),"--", linewidth = 2, label = "χ=2 cmpo")
+xlabel("ω")
+ylabel("S(ω) difference (abs)")
+legend()
+title(@sprintf "β = %i" β)
+PyPlot.savefig("atomic_struc_diff.pdf",bbox_inches="tight")
