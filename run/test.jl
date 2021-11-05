353d6e8839e154ab8495a4b9837159ce0fe66e40
sliang <sliang@iphy.ac.cn>
2021-11-05 10:44:18 +0800

    add Zygote package

--- run/test.jl
+++ run/test.jl
@@ -3,6 +3,7 @@ Pkg.activate("../")
 using cMPO
 using Optim
 using Printf
+using Zygote
 
 function converttime(t::Number)
     (m, s) = divrem(t, 60)
@@ -21,7 +22,7 @@ function converttime(t::Number)
     end
 end
 
-println("2021-11-02:test NewtonTrustRegion time")
+println("2021-11-04:test NewtonTrustRegion time")
 println("χ = 8; β = 1.0")
 χ = 8; β = 1.0
 w = TFIsing(1.0, 1.0);
@@ -30,9 +31,10 @@ arr = init_cmps(χ,w) |> toarray;
 f1 = x -> free_energy(x, w, β);
 f2 = x -> free_energy(x, dim, w, β);
 g1 = gradient_function(f1);
+h1 = hessian_function(f1)
 
 g2 = gradient_function(f2);
-h = hessian_function(f2);
+h2 = hessian_function(f2);
 
 
 t1 = time();
@@ -45,3 +47,14 @@ t1 = time();
 op2 = optimize(f2, g2, h, vec, NewtonTrustRegion());
 t2 = time();
 println("NewtonTrustRegion ", converttime(t2-t1))
+
+
+"""
+    hessian_reverse(f, x)
+This should be equivalent to [`hessian(f, x)`](@ref hessian),
+but implemented using reverse over reverse mode, all Zygote.
+(This is usually much slower, and more likely to find errors.)
+"""
+hessian_reverse(f, x::AbstractArray) = jacobian(x -> gradient(f, x)[1], x)[1]
+
+hessian_reverse(f, x::Number) = gradient(x -> gradient(f, x)[1], x)[1]
