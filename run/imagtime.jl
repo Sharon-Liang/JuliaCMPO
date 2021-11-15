e70de508871de2ce9fdec4f0461348d396da1530
sliang <sliang@iphy.ac.cn>
2021-11-15 15:20:50 +0800

    update

--- run/imagtime.jl
+++ run/imagtime.jl
@@ -6,17 +6,16 @@ using DelimitedFiles
 using JLD, HDF5
 using Printf
 
-println("2021-11-15: G(τ) and g(iωn): D=8")
-
-D = 8
-z1 = make_operator(pauli(:z), D)
-z2 = make_operator(pauli(:z), 2D)
-
+println("2021-11-15: G(τ) and g(iωn): D=8, 16")
 N = 40
 beta = [10.0, 20.0, 30.0, 40.0]
 gamma = [1.0, 1.5, 2.0]
-
 len = 1601
+
+println("D = 8 begin!")
+D = 8
+z1 = make_operator(pauli(:z), D)
+z2 = make_operator(pauli(:z), 2D)
 
 for j = 1:length(gamma)
     g = gamma[j]; w = TFIsing(1.,g)
@@ -71,8 +70,8 @@ for j = 1:length(gamma)
         path2 = @sprintf "../data/imagtime/gdivwn_g_%.1f_D_2m%i_beta_%i.txt" g D β
 
         ωn = [Masubara_freq(i,β) for i=1:N]
-        Gt1 = [Masubara_freq_T1(i,z1,z1,ψ1,w,β) for i=1:N]
-        Gt2 = [Masubara_freq_T1(i,z2,z2,ψ2,w,β) for i=1:N]  
+        Gt1 = [Masubara_freq_GFdivOmega(i,z1,z1,ψ1,w,β) for i=1:N]
+        Gt2 = [Masubara_freq_GFdivOmega(i,z2,z2,ψ2,w,β) for i=1:N]  
 
         open(path1,"w") do file
             for i=1:N
@@ -88,6 +87,84 @@ for j = 1:length(gamma)
     end
     println("finish Γ/J = ", g)
 end
+
+
+println("D = 16 begin!")
+D = 16
+z1 = make_operator(pauli(:z), D)
+z2 = make_operator(pauli(:z), 2D)
+
+for j = 1:length(gamma)
+    g = gamma[j]; w = TFIsing(1.,g)
+    dpath = @sprintf "../data/chi16/ug_%.1f.jld" g
+    d1 = load(dpath)
+    for b = 1:length(beta)
+        β = beta[b]; key = string(β)
+        ψ1 = d1[key][2] |> tocmps; ψ2 = w * ψ1
+        
+        # G(τ)
+        path1 = @sprintf "../data/imagtime/gtau_g_%.1f_D_%i_beta_%i.txt" g D β
+        path2 = @sprintf "../data/imagtime/gtau_g_%.1f_D_2m%i_beta_%i.txt" g D β
+
+        tau = [i for i in range(0, β, length = len)]
+        Gt1 = [correlation_2time(i,z1,z1,ψ1,w,β) for i in tau]
+        Gt2 = [correlation_2time(i,z2,z2,ψ2,w,β) for i in tau]  
+
+        open(path1,"w") do file
+            for i=1:len
+                writedlm(file,[tau[i]/β Gt1[i]])
+            end
+        end
+    
+        open(path2,"w") do file
+            for i=1:len
+                writedlm(file,[tau[i]/β Gt2[i]])
+            end
+        end
+
+        # G(ωn)
+        path1 = @sprintf "../data/imagtime/giwn_g_%.1f_D_%i_beta_%i.txt" g D β
+        path2 = @sprintf "../data/imagtime/giwn_g_%.1f_D_2m%i_beta_%i.txt" g D β
+
+        ωn = [Masubara_freq(i,β) for i=1:N]
+        Gt1 = [Masubara_freq_GF(i,z1,z1,ψ1,w,β) for i=1:N]
+        Gt2 = [Masubara_freq_GF(i,z2,z2,ψ2,w,β) for i=1:N]  
+
+        open(path1,"w") do file
+            for i=1:N
+                writedlm(file,[ωn[i] real(Gt1[i]) imag(Gt1[i])])
+            end
+        end
+    
+        open(path2,"w") do file
+            for i=1:N
+                writedlm(file,[ωn[i] real(Gt2[i]) imag(Gt2[i])])
+            end
+        end
+
+        # χ(ωn)/ωn
+        path1 = @sprintf "../data/imagtime/gdivwn_g_%.1f_D_%i_beta_%i.txt" g D β
+        path2 = @sprintf "../data/imagtime/gdivwn_g_%.1f_D_2m%i_beta_%i.txt" g D β
+
+        ωn = [Masubara_freq(i,β) for i=1:N]
+        Gt1 = [Masubara_freq_GFdivOmega(i,z1,z1,ψ1,w,β) for i=1:N]
+        Gt2 = [Masubara_freq_GFdivOmega(i,z2,z2,ψ2,w,β) for i=1:N]  
+
+        open(path1,"w") do file
+            for i=1:N
+                writedlm(file,[ωn[i] real(Gt1[i]) imag(Gt1[i])])
+            end
+        end
+    
+        open(path2,"w") do file
+            for i=1:N
+                writedlm(file,[ωn[i] real(Gt2[i]) imag(Gt2[i])])
+            end
+        end
+    end
+    println("finish Γ/J = ", g)
+end
+
 
 """
 #==free_energy compare==#
