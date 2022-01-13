3080dca722028ed276eb52cdd3abdd7f215d84ec
Sharon-Liang <liangshuang228@gmail.com>
2022-01-13 10:16:56 +0800

    change folder

---
+++ spectral/sw_pade.jl
@@ -0,0 +1,19 @@
+include("./StructureFactorTrain.jl")
+using DelimitedFiles, Printf
+using Polynomials
+
+g = 1.0
+β = 10.0
+D = 8
+ω = build_range(0.01, 10)
+
+#load χ(τ) and dχ(τ) data
+p1 = @sprintf "./data/ising/imagtime/gtau/g_%.1f_D_%i_beta_%i.txt" g D β
+p2 = @sprintf "./data/ising/imagtime/dgtau/g_%.1f_D_%i_beta_%i.txt" g D β
+
+d1 = readdlm(p1)
+d2 = readdlm(p2)
+
+x1 = d1[:,1]; y1 = d1[:,2]; data1 = [(x1, y1)]
+x2 = d2[:,1]; y2 = d2[:,2]; data2 = [(x1, y1, x2, y2)]
+
