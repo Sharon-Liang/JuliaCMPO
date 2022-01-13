67872b30d48f27bf5886528bf085dd07bacf018a
Sharon-Liang <liangshuang228@gmail.com>
2022-01-13 16:40:21 +0800

    nnls fail: s(w) = 0 except for w=0

--- spectral/sw_NNLS.jl
+++ spectral/sw_NNLS.jl
@@ -2,7 +2,6 @@ include("./StructureFactorTrain.jl")
 using DelimitedFiles, Printf
 using NonNegLeastSquares
 
-
 g = 1.0
 β = 10.0
 D = 8
@@ -18,3 +17,11 @@ d2 = readdlm(p2)
 x1 = d1[:,1]; y1 = d1[:,2]; data1 = [(x1, y1)]
 x2 = d2[:,1]; y2 = d2[:,2]; data2 = [(x1, y1, x2, y2)]
 
+
+#build kernel
+dω = 0.01
+ωmax = 10
+ω = build_range(dω, ωmax)
+
+K0 = build_kernal(0, x1, ω, β)
+@time S = nonneg_lsq(K0,y1;alg=:nnls) 
