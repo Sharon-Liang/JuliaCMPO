b643f23bd124b8bbef2d96caa9b3fd653e8b1ae1
Sharon-Liang <liangshuang228@gmail.com>
2022-01-13 15:21:14 +0800

    L-BFGS data

--- spectral/sw_LBFGS.jl
+++ spectral/sw_LBFGS.jl
@@ -2,6 +2,7 @@ include("./StructureFactorTrain.jl")
 using DelimitedFiles, Printf
 using Optim, Zygote
 using FluxOptTools
+using BSON: @save, @load
 
 g = 1.0
 β = 10.0
@@ -22,6 +23,63 @@ x2 = d2[:,1]; y2 = d2[:,2]; data2 = [(x1, y1, x2, y2)]
 model, pars = build_NN_model(5)
 s(ω::Real) = model([ω])[1]
 
+loss(data1[1],β,s)
+
+dir = "./spectral/data/chi"
+isdir(dir) || mkdir(dir)
+#save the init model
+mpi = @sprintf "%s/LBFGS_g_%.1f_D_%i_beta_%i_init.bson" dir g D β 
+@save mpi model
+
 Zygote.refresh() # currently needed when defining new adjoints
-lossfun, gradfun, fg!, p0 = optfuns(()->loss(data1[1], β, s), pars)
-res = Optim.optimize(Optim.only_fg!(fg!), p0, LBFGS(), Optim.Options(iterations=10000, store_trace=true))
+@time lossfun, gradfun, fg!, p0 = optfuns(()->loss(data1[1], β, s), pars)
+@time res = Optim.optimize(Optim.only_fg!(fg!), p0, 
+            Optim.Options(iterations=3000, store_trace=true, show_trace=true))
+
+mp = @sprintf "%s/LBFGS_g_%.1f_D_%i_beta_%i.bson" dir g D β 
+@save mp model
+sp = @sprintf "%s/g_%.1f_D_%i_beta_%i.txt" dir g D β
+open(sp, "w") do file
+    for i = 1:length(ω)
+        writedlm(file, [ω[i] s(ω[i])])
+    end
+end
+
+trace = Optim.f_trace(res)
+ep = @sprintf "%s/ftrace_g_%.1f_D_%i_beta_%i.txt" dir g D β
+open(ep, "w") do file
+    writedlm(file, trace)
+end
+
+#build NN model: dchi
+model, pars = build_NN_model(5)
+s(ω::Real) = model([ω])[1]
+loss(data2[1],β,s)
+
+dir = "./spectral/data/dchi"
+isdir(dir) || mkdir(dir)
+
+#save the init model
+mpi = @sprintf "%s/LBFGS_g_%.1f_D_%i_beta_%i_init.bson" dir g D β 
+@save mpi model
+
+
+Zygote.refresh() # currently needed when defining new adjoints
+@time lossfun, gradfun, fg!, p0 = optfuns(()->loss(data2[1], β, s), pars)
+@time res = Optim.optimize(Optim.only_fg!(fg!), p0, LBFGS(),
+            Optim.Options(iterations=3000, store_trace=true, show_trace=true))
+
+mp = @sprintf "%s/LBFGS_g_%.1f_D_%i_beta_%i.bson" dir g D β 
+@save mp model
+sp = @sprintf "%s/g_%.1f_D_%i_beta_%i.txt" dir g D β
+open(sp, "w") do file
+    for i = 1:length(ω)
+        writedlm(file, [ω[i] s(ω[i])])
+    end
+end
+            
+trace = Optim.f_trace(res)
+ep = @sprintf "%s/ftrace_g_%.1f_D_%i_beta_%i.txt" dir g D β
+open(ep, "w") do file
+    writedlm(file, trace)
+end
