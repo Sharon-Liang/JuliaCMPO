6ffc04907c077f0f352ba1f61181fd33f1f92e25
Sharon-Liang <sliang@iphy.ac.cn>
2022-02-21 16:13:50 +0800

    modify kernel function

--- src/KernelFunction.jl
+++ src/KernelFunction.jl
@@ -3,20 +3,26 @@ using ChainRulesCore
 """
     Kernal functions corresponding to S(ω)
 """
-function kernel(n::Integer, τ::Real, ω::Real, β::Real)
+function kernel(n::Integer, τ::Real, ω::Real, β::Real; spectrum::Symbol=:S)
+    if spectrum == :S den = 1.0
+    elseif spectrum == :A den = 1.0 - exp(-β*ω)
+    else @error "spectrum should be :S or :A"
+    end
     res =  (-1)^n * exp(-τ*ω) + exp(-(β-τ)*ω)
-    return ω^n * res / 2π
+    res = ω^n * res / 2π
+    return res/den
 end
 
-function build_kernal(n::Integer, τ::AbstractVector, ω::AbstractVector, β::Real)
+function build_kernal(n::Integer, τ::AbstractVector, ω::AbstractVector, β::Real, spectrum::Symbol = :S)
     dω = ω[2] - ω[1]
     K = zeros(length(τ), length(ω))
     for i = 1:length(τ), j = 1:length(ω)
-        K[i,j] = kernel(n, τ[i], ω[j], β)*dω
+        K[i,j] = kernel(n, τ[i], ω[j], β, spectrum = spectrum)*dω
     end
     return K
 end
 @non_differentiable build_kernal(n::Integer, τ::AbstractVector, ω::AbstractVector, β::Real)
+@non_differentiable build_kernal(n::Integer, τ::AbstractVector, ω::AbstractVector, β::Real, spectrum::Symbol)
 
 
 """
