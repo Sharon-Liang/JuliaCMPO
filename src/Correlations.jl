8a97fd904e49920a2b7206e734b9f65464b78f5b
Sharon-Liang <sliang@iphy.ac.cn>
2022-03-03 16:01:39 +0800

    modify G(iwn), add Lehmann Sw, Aw

--- src/Correlations.jl
+++ src/Correlations.jl
@@ -5,7 +5,7 @@
 function correlation_2time(τ::Number, A::AbstractMatrix,B::AbstractMatrix,
                             ψl::CMPS, ψr::CMPS, W::CMPO, β::Real)
     K = ψl * W * ψr 
-    e, v = eigensolver(K)
+    e, v = symeigen(K)
     min = minimum(e); e = e .- min
     A = v' * A * v
     B = v' * B * v
@@ -20,61 +20,99 @@ correlation_2time(τ::Number, A::AbstractMatrix, B::AbstractMatrix, ψ::CMPS, W:
     correlation_2time(τ, A, B, ψ, ψ, W, β)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 """
-A useful function: f = e^(-b*e1) - e^(-b*e2) / (e2 - e1)
+    Masubara frequency Green's functions: defalt type = :b
+    G(iωn) = 1/Z ∑ Anm Bmn (exp(-βEn) - λexp(-βEm)) / (iωn - Em + En)
 """
-function diffaddexp(b::Real, e1::Real, e2::Real)
-    if abs(e2 - e1) < 1.e-10
-        return exp(-b*e1) * b
+function LehmannGFKernel(z::Number, En::Real, Em::Real, β::Real; type::Symbol=:b)
+    if type == :b
+        λ = 1.0
+    elseif type == :f 
+        λ = -1.0
     else
-        num = exp(-b*e1) - exp(-b*e2)
-        den = e2 - e1
+        @error "type should be :b for bosons and :f for fermions"
+    end
+
+    if type ==:b && z==0 && abs(En - Em) < 1.e-10
+        return -β*exp(-β*Em)
+    else
+        num = exp(-β*En) - λ*exp(-β*Em)
+        den = z - Em + En
         return num/den
     end
 end
 
-"""
-Masubara frequency Green's functions: defalt type = :b
-"""
 function Masubara_freq_GF(n::Integer, A::AbstractMatrix,B::AbstractMatrix,
-                        ψ::CMPS, W::CMPO, β::Real)
-    λ = 1.0
-    ωn = Masubara_freq(n,β,type=:b)
-    K = ψ * W * ψ 
-    e, v = eigensolver(K)
+                        ψl::CMPS, ψr::CMPS, W::CMPO, β::Real; type::Symbol = :b)
+    K = ψl * W * ψr
+    e, v = symeigen(K)
     min = minimum(e); e = e .- min
     A = v' * A * v
     B = v' * B * v
     den = exp.(-β * e) |> sum
     num = 0.0
-    # sum i != j , add i = j term when ωn = 0
-    if ωn != 0
-        for i = 1: length(e), j = 1: length(e)
-            up = exp(-β*e[i]) - λ*exp(-β*e[j])
-            up = up * A[i,j] * B[j,i]
-            down = 1.0im * ωn - e[j] + e[i]
-            num += up/down
-        end
-    else
-        for i = 1: length(e), j = 1: length(e)
-            num -= A[i,j] * B[j,i]* diffaddexp(β,e[i],e[j])
-        end
+    iωn = 1.0im * Masubara_freq(n,β,type=type)
+    for i = 1: length(e), j = 1: length(e)
+        num += A[i,j] * B[j,i] * LehmannGFKernel(iωn, e[i], e[j], β, type=type)
     end
     return num/den
 end
+Masubara_freq_GF(n::Integer, A::AbstractMatrix,B::AbstractMatrix, 
+    ψ::CMPS, W::CMPO, β::Real; type::Symbol = :b) =
+    Masubara_freq_GF(n, A, B, ψ, ψ, W, β, type = type)
 
-#add spectrum Function
+
+function Lehmann_spectral_function(ω::Real, A::AbstractMatrix,B::AbstractMatrix,
+                                ψl::CMPS, ψr::CMPS, W::CMPO, β::Real; η::Real=0.01, type::Symbol = :b)
+    if type == :b
+        λ = 1.0
+    elseif type == :f 
+        λ = -1.0
+    else
+        @error "type should be :b for bosons and :f for fermions"
+    end
+    K = ψl * W * ψr
+    e, v = symeigen(K)
+    min = minimum(e); e = e .- min
+    A = v' * A * v
+    B = v' * B * v
+    den = exp.(-β * e) |> sum
+    num = 0.0
+    for i = 1: length(e), j = 1: length(e)
+        num += A[i,j] * B[j,i] * 
+                (exp(-β*e[i]) - λ*exp(-β*e[j])) * 
+                delta(ω - e[j] + e[i], η)
+    end
+    return 2π*num/den
+end
+Lehmann_spectral_function(ω::Real, A::AbstractMatrix,B::AbstractMatrix, 
+    ψ::CMPS, W::CMPO, β::Real; η::Real=0.01, type::Symbol = :b) =
+    Lehmann_spectral_function(ω, A, B, ψ, ψ, W, β, η=η, type = type)
+
+
+function Lehmann_structure_factor(ω::Real, A::AbstractMatrix,B::AbstractMatrix,
+        ψl::CMPS, ψr::CMPS, W::CMPO, β::Real; η::Real=0.01, type::Symbol = :b)
+    if type == :b
+        λ = 1.0
+    elseif type == :f 
+        λ = -1.0
+    else
+        @error "type should be :b for bosons and :f for fermions"
+    end
+    K = ψl * W * ψr
+    e, v = symeigen(K)
+    min = minimum(e); e = e .- min
+    A = v' * A * v
+    B = v' * B * v
+    den = exp.(-β * e) |> sum
+    num = 0.0
+    for i = 1: length(e), j = 1: length(e)
+        num += A[i,j] * B[j,i] * exp(-β*e[i]) * delta(ω - e[j] + e[i], η)
+    end
+    return 2π*num/den
+end
+Lehmann_structure_factor(ω::Real, A::AbstractMatrix,B::AbstractMatrix, 
+        ψ::CMPS, W::CMPO, β::Real; η::Real=0.01, type::Symbol = :b) =
+    Lehmann_structure_factor(ω, A, B, ψ, ψ, W, β, η=η, type = type)
 
 #end  # module Correlations
