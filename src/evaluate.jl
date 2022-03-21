ed3fff010d80b6b3f4d96b1f619dfd5138606fab
Sharon-Liang <sliang@iphy.ac.cn>
2022-03-21 10:36:10 +0800

    modify opt summary file

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -17,7 +17,8 @@ end
 """
     Evaluate PhysModel m when its transfer matrix is hermitian
 """
-function hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFolder::String; init::Union{CMPS, Nothing} = nothing)
+function hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFolder::String; 
+            init::Union{CMPS, Nothing} = nothing)
     """
     ResultFolder: classified by Model parameters(interaction, width), store CMPS and Obsv files
     CMPSResultFolder: CMPS information, classified by bond dimension
@@ -33,19 +34,26 @@ function hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFolder
 
     ψ = ψ |> diagQ
     loss() = free_energy(CMPS(diag(ψ.Q)|> diagm, ψ.R), m.Tmatrix, β)
+    F_initial = loss()
 
     p0, f, g! = optim_functions(loss, Params([ψ.Q, ψ.R]))
-    opt = Optim.optimize(f, g!, p0, LBFGS(),Optim.Options(iterations=10000))
-    
+    opt = Optim.optimize(f, g!, p0, LBFGS(),
+            Optim.Options(iterations=10000), store_trace = true, show_every = 10)
+    F_final = minimum(opt)
+
     #save optimize result
     OptResultFile = @sprintf "%s/beta_%.2f.txt" OptResultFolder β
     open(OptResultFile, "w") do file
+        F_initial_string = @sprintf "F_initial = %.16f \n" F_initial
+        F_final_string =   @sprintf "F_final   = %.16f \n" F_final
+        write(file, F_initial_string)
+        write(file, F_final_string)
         write(file, opt)
     end
 
     # calculate thermal dynamic quanties
     dict = Dict()
-    dict["F"] = minimum(opt)
+    dict["F"] = F_final
     ResultFile = @sprintf "%s/beta_%.2f.hdf5" CMPSResultFolder β
     saveCMPS(ResultFile, ψ, dict)
     return ψ, dict
@@ -57,7 +65,7 @@ end
     or force to do power projection 
 """
 function non_hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFolder::String; 
-                                init::Union{CMPS, Nothing} = nothing, max_pow_step::Integer = 100)
+                                init::Union{CMPS, Nothing} = nothing, max_pow_step::Integer = 50)
     """
         ResultFolder: classified by Model parameters(interaction, width), store CMPS and Obsv files
         CMPSResultFolder: CMPS information, classified by bond dimension
@@ -81,9 +89,14 @@ function non_hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFo
             writedlm(cfile, [pow_step free_energy(m.Ut*ψ, ψ, m.Tmatrix, β)])
 
             ψ0 = m.Tmatrix * ψ
-            ψ, opt = compress_cmps(ψ0, bondD, β, init = ψ, return_opt = true)
-            OptResultFile = @sprintf "%s/cmps_step_%03i.txt" ChkpFolder β
+            ψ, fidelity_initial, fidelity_final, opt = compress_cmps(ψ0, bondD, β, init = ψ, return_opt = true)
+            
+            OptResultFile = @sprintf "%s/opt_summary_step_%03i.txt" ChkpFolder pow_step
             open(OptResultFile, "w") do file
+                fidel_initial_string = @sprintf "fidelity_initial = %.16f \n" fidelity_initial
+                fidel_final_string =   @sprintf "fidelity_final   = %.16f \n" fidelity_final
+                write(file, fidel_initial_string)
+                write(file, fidel_final_string)
                 write(file, opt)
             end
         end
@@ -92,7 +105,6 @@ function non_hermitian_evaluate(m::PhysModel, bondD::Integer, β::Real, ResultFo
     # calculate thermal dynamic quanties
     dict = Dict()
     dict["F"] = free_energy(m.Ut*ψ, ψ, m.Tmatrix, β)
-
     ResultFile = @sprintf "%s/beta_%.2f.hdf5" CMPSResultFolder β
     saveCMPS(ResultFile, ψ, dict)
     return ψ, dict
