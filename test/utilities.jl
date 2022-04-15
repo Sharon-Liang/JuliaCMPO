358be7b0c27e28147e71666ba8c700a9253d0258
Sharon-Liang <sliang@iphy.ac.cn>
2022-04-15 19:54:08 +0800

    test CUDA functions

--- test/utilities.jl
+++ test/utilities.jl
@@ -2,29 +2,23 @@ using cMPO, Test
 using LinearAlgebra
 using Random; Random.seed!()
 
-@testset "utilities for hermitian matrices" begin
-    D = 4; T = ComplexF64
-    for A in [randn(D,D), randn(T,D,D)], device in [:cpu, :gpu]
-        sa = symmetrize(A)
-        tr_exp = exp(sa) |> tr |> real
-        log_trexp = log(tr_exp)
-
-        @test ishermitian(sa)
-        @test logtrexp(A, device=device) ≈ log_trexp
-    end
+function cpu_solver(f::Function, M::AbstractArray...)
+    return f(M...)
 end
 
+function gpu_solver(f::Function, M::AbstractArray...)
+    M = Tuple(convert(CuArray, x) for x in M)
+    eltype(M) <: CuArray ? (return f(M...)) : error("CuArray conversion fails")
+end
 
-@testset "-β * eigvals(A) |> sum and eigvals(-β * A) |> sum" begin
+@testset "utilities for hermitian matrices" begin
     D = 4; T = ComplexF64
-    β = 10
     for A in [randn(D,D), randn(T,D,D)]
-        A = A |> symmetrize |> Hermitian
-        e1 = eigvals(A)
-        e2 = eigvals(-β * A)
-        a1 = exp.(-β .* e1) |> sum
-        a2 = exp.(e2) |> sum
-        @test a1 ≈ a2
+        log_trexp = symmetrize(A) |> exp |> tr |> real |> log
+        for solver in [cpu_solver, gpu_solver]
+            @test ishermitian(solver(symmetrize, A))
+            @test solver(logtrexp, A) ≈ log_trexp
+        end
     end
 end
 
