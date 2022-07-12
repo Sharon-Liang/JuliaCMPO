8afcd5fb5f68350741d4c19c6185692f45c45e77
Sharon-Liang <sliang@iphy.ac.cn>
2022-07-12 14:57:32 +0800

    fix: eigensolver and eigsolve

--- src/eigensolver.jl
+++ src/eigensolver.jl
@@ -1,25 +1,4 @@
-@reexport import FiniteTLanczos.eigensolver
 @reexport import KrylovKit.eigsolve
-
-for elty in (:Float32, :Float64)
-    """ 
-    eigensolver for CuMatrix
-    For CUDA dense matrix eigen solver `CUSOLVER.syevd!` and `CUSOLVER.heevd!`:
-        'N'/'V': return eigenvalues/both eigenvalues and eigenvectors 
-        'U'/'L': Upper/Lower triangle of `M` is stored.
-    """
-    @eval eigensolver(M::CuMatrix{$elty}) = CUSOLVER.syevd!('V', 'U', M)
-end
-
-for elty in (:ComplexF32, :ComplexF64)
-    """ 
-    eigensolver for CuMatrix
-    For CUDA dense matrix eigen solver `CUSOLVER.syevd!` and `CUSOLVER.heevd!`:
-        'N'/'V': return eigenvalues/both eigenvalues and eigenvectors 
-        'U'/'L': Upper/Lower triangle of `M` is stored.
-    """
-    @eval eigensolver(M::CuArray{$elty}) = CUSOLVER.heevd!('V', 'U', M)
-end
 
 eigsolve(M::CMPSMatrix{T}, howmany, which; args...) where {T} =
     eigsolve(M, size(M,1), howmany, which, T, ishermitian = true, args...)
--- src/solver.jl
+++ src/solver.jl
@@ -1,20 +1,18 @@
-function cpu_solver(f::Function, M::AbstractArray...)
-    M = map(x->convert(Array, x), M)
-    return f(M...)
-end
-
-function gpu_solver(f::Function, M::AbstractArray...)
-    M = map(x->convert(CuArray, x), M)
-    return f(M...)
-end
 
 TensorUnion = Union{AbstractCTensor, CMPSMatrix}
+
+"""
+    `cpu_solver` for CTensors and CMPSMatrix
+"""
 function cpu_solver(f::Function, M::T...) where T<:TensorUnion
     #M = Tuple(CTensor(x) for x in M)
     M = map(x->CTensor(x), M)
     return f(M...)
 end
 
+"""
+    `gpu_solver` for CTensors and CMPSMatrix
+"""
 function gpu_solver(f::Function, M::T...) where T<:TensorUnion
     #M = Tuple(CuCTensor(x) for x in M)
     M = map(x->CuCTensor(x), M)
