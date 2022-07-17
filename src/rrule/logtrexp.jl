0da58fb6f5d7e6736555dbaa5460cc8c84e26561
Sharon-Liang <sliang@iphy.ac.cn>
2022-07-17 14:55:34 +0800

    fix: adjpint for CMPSMatrix

--- src/rrule/logtrexp.jl
+++ src/rrule/logtrexp.jl
@@ -1,11 +1,5 @@
 Zygote.@adjoint Array(x::CuArray) = Array(x), dy->(CuArray(dy),)
-
-function ChainRules.rrule(::Type{CMPSMatrix}, ψl::AbstractCMPS{T, S, U}, 
-                        ψr::AbstractCMPS{T, S, U}) where {T,S,U}
-    CMPSMatrix_pullback(ȳ) = ChainRules.NoTangent(), ȳ.ψl, ȳ.ψr
-    return CMPSMatrix(ψl, ψr), CMPSMatrix_pullback
-end
-
+Zygote.@adjoint CMPSMatrix(ψl, ψr) = CMPSMatrix(ψl, ψr), ȳ->(ȳ.ψl, ȳ.ψr)
 Zygote.@adjoint CTensor(x::T) where T<:CMPSMatrix = CTensor(x), ȳ->(CTensor(CMPSMatrix(ȳ.ψl, ȳ.ψr)), )
 Zygote.@adjoint CuCTensor(x::T) where T<:CMPSMatrix = CuCTensor(x), ȳ->(CuCTensor(CMPSMatrix(ȳ.ψl, ȳ.ψr)),)
 
