ba3fb4adfebf867cf3c9ca9a424796de32194ba4
Sharon-Liang <sliang@iphy.ac.cn>
2023-02-20 19:06:50 +0800

    test: ising ,add heisenberg

---
+++ test/evaluate-heisenberg.jl
@@ -0,0 +1,38 @@
+using Test, JuliaCMPO
+using JLD, DelimitedFiles
+using LinearAlgebra
+
+processor = CPU
+obsv_functions = [free_energy]
+
+
+#=
+### *HeisenbergChain* : *Power method*
+=#
+βlist = [1.0, 2.0]
+Tₘ = model(XXZChain(), 1.0)
+
+#dtrg result with bond dimension 256
+Oe = zeros(2, 2)
+Oe[:, 1] = [1.000000, -0.7953882215614290]
+Oe[:, 2] = [2.000000, -0.5385576248164411] 
+
+
+#=
+### *HeisenbergChain* : *Power method*
+=#
+@testset "HeisenbergChain_power" begin
+    bondD = 8
+    result_folder = "./test/HeisenbergChain_power"
+    
+    power_evaluate(Tₘ, bondD, βlist; processor, obsv_functions, result_folder, max_pow_step = 50)
+    Oc = readdlm(result_folder*"/obsvs.txt", skipstart = 1)
+    @test Oc ≈ Oe rtol = 1.e-3
+
+    to_group = 2
+    result_folder = "./test/HeisenbergChain_power_group"
+    power_evaluate(Tₘ, bondD, βlist; processor, obsv_functions, result_folder, max_pow_step = 50, to_group)
+    Og = readdlm(result_folder*"/obsvs.txt", skipstart = 1)
+    @test Og ≈ Oe rtol = 1.e-3
+    @test norm(Og - Oe) < norm(Oc - Oe)
+end
--- test/evaluate-ising.jl
+++ test/evaluate-ising.jl
@@ -1,6 +1,5 @@
 using Test, JuliaCMPO
 using JLD, DelimitedFiles
-using LinearAlgebra
 
 processor = CPU
 obsv_functions = [free_energy, energy, entropy]
@@ -20,7 +19,6 @@ Oe[2, :] = [1.50, -1.3338698801984272, -1.2073855733208403, 0.1897264603163804]
 #=
 ### *TFIsingChain* : *Variational Principle*
 =#
-#=
 @testset "TFIsingChain_variation" begin
     bondD = 8
     result_folder = "./test/TFIsingChain_variation"
@@ -29,7 +27,7 @@ Oe[2, :] = [1.50, -1.3338698801984272, -1.2073855733208403, 0.1897264603163804]
     Oc = readdlm(result_folder*"/obsvs.txt", skipstart = 1)
     @test Oc ≈ Oe rtol = 1.e-5
 end
-=#
+
 
 
 
@@ -56,30 +54,3 @@ end
     @test Oc ≈ Oe rtol = 1.e-3
 end
 
-
-#=
-### *HeisenbergChain* : *Power method*
-=#
-βlist = [1.0, 2.0]
-obsv_functions = [free_energy]
-Tₘ = model(XXZChain(), 1.0)
-
-#dtrg result with bond dimension 256
-Oe = zeros(2, 2)
-Oe[:, 1] = [1.000000, -0.7953882215614290]
-Oe[:, 2] = [2.000000, -0.5385576248164411] 
-@testset "HeisenbergChain_power" begin
-    bondD = 8
-    result_folder = "./test/HeisenbergChain_power"
-    
-    power_evaluate(Tₘ, bondD, βlist; processor, obsv_functions, result_folder, max_pow_step = 50)
-    Oc = readdlm(result_folder*"/obsvs.txt", skipstart = 1)
-    @test Oc ≈ Oe rtol = 1.e-3
-
-    to_group = 2
-    result_folder = "./test/HeisenbergChain_power_group"
-    power_evaluate(Tₘ, bondD, βlist; processor, obsv_functions, result_folder, max_pow_step = 50, to_group)
-    Og = readdlm(result_folder*"/obsvs.txt", skipstart = 1)
-    @test Og ≈ Oe rtol = 1.e-3
-    @test norm(Og - Oe) < norm(Oc - Oe)
-end
