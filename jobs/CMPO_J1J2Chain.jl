f7cc4d403e8ce06cc0826a798e35247f1fc37dba
Sharon-Liang <sliang@iphy.ac.cn>
2023-02-24 13:39:09 +0800

    fix : update job file

--- jobs/CMPO_J1J2Chain.jl
+++ jobs/CMPO_J1J2Chain.jl
@@ -19,18 +19,10 @@ settings = ArgParseSettings(prog="CMPO: 1D TFIsing model"
         arg_type = Float64
         default = 1.0
         help = "Transverse Field strength: Γ"
-    "--bi"
+    "--beta"
         arg_type = Float64
         default = 1.0
-        help = "initial value of β: inverse temperature"
-    "--bf"
-        arg_type = Float64
-        default = 30.0
-        help = "final value of β: inverse temperature" 
-    "--bstep"
-        arg_type = Float64
-        default = 1.0
-        help = "step size of β: inverse temperature" 
+        help = "β: inverse temperature"
     "--bondD"
         arg_type = Int64
         default = 8
@@ -47,6 +39,14 @@ settings = ArgParseSettings(prog="CMPO: 1D TFIsing model"
         arg_type = String
         default = Dates.format(now(), "yyyy-mm-dd")
         help = "date tag"
+    "--group"
+        arg_type = Int
+        default = 0
+        help = "size of unit cell"
+    "--shift"
+        arg_type = Float64
+        default = 0.
+        help = "shift spectrum"
 end
 
 parsed_args = parse_args(settings; as_symbols=true)
@@ -54,27 +54,31 @@ print(parsed_args,"\n")
 
 const J1 = parsed_args[:J1]
 const J2 = parsed_args[:J2]
-const bi = parsed_args[:bi]
-const bf = parsed_args[:bf]
-const bstep = parsed_args[:bstep]
+const β = parsed_args[:beta]
 const bondD = parsed_args[:bondD]
 const ResultFolder = parsed_args[:ResultFolder]
 const tag = parsed_args[:tag]
 const processor = Processor(parsed_args[:processor])
 
+const to_group = parsed_args[:group]
+const to_shift = parsed_args[:shift]
+
 
 const wid = 1
 
 isdir(ResultFolder) || mkdir(ResultFolder)
-ModelResultFolder = @sprintf "%s/J1_%f_J2_%f_wid_%02i_bondD_%02i_%s" ResultFolder J1 J2 wid bondD tag
+
+ModelResultFolder = @sprintf "%s/J1_%f_J2_%f_wid_%02i_bondD_%02i_group_%i_shift_%e_%s" ResultFolder J1 J2 wid bondD to_group to_shift tag 
+isdir(ModelResultFolder) || mkdir(ModelResultFolder)
+
+ModelResultFolder = @sprintf "%s/beta_%f" ModelResultFolder β
 isdir(ModelResultFolder) || mkdir(ModelResultFolder)
    
 #CMPO
 Tₘ = model(J1J2Chain(), J1, J2)
-βlist = 1 ./ [i for i in range(0.03, 0.05, step=0.005)]
 
 @timeit to "evaluate" begin
-    power_evaluate(Tₘ, bondD, βlist; processor, result_folder = ModelResultFolder, to_shift = 1.e-3)
+    power_evaluate(Tₘ, bondD, [β]; processor, result_folder = ModelResultFolder, to_shift, to_group)
 end
 
 const End_Time = Dates.format(now(), "yyyy-mm-dd HH:MM:SS")
--- jobs/jobsCMPO_J1J2Chain.jl
+++ jobs/jobsCMPO_J1J2Chain.jl
@@ -5,7 +5,7 @@ phys_model = "J1J2Chain"
 env = "/home/sliang/JuliaCode/JuliaCMPO"
 prog = env * "/jobs/CMPO_$(phys_model).jl"
 
-processor = CPU
+processor = GPU
 machine = titanv
 
 logtag = Dates.format(now(), "yyyy-mm-dd")*"-"*"$(processor)"
@@ -15,31 +15,38 @@ cpu_per_task = 8
 tag = Dates.format(now(), "yyyy-mm-dd")*"-"*"$(processor)"
 
 J1list = [1.0]
-J2list = [0.5]
+J2list = [0.241167, 0.5]
 bondDlist = [20]
+βlist = [20, 30]
+to_group = 2
+to_shift = 0.
 
 
 #CREAT LOG FOLDER
 logdir = "/data/sliang/JuliaCMPO/log"
 isdir(logdir) || mkdir(logdir)
 
-for J1 in J1list, J2 in J2list, bondD in bondDlist
+for J1 in J1list, J2 in J2list, bondD in bondDlist, β in βlist
         args = Dict("J1"=>J1,
                     "J2"=>J2,
                     "bondD"=>bondD,
+                    "beta"=>β,
                     "tag"=>tag,
+                    "group"=>to_group,
+                    "shift"=>to_shift,
                     "processor"=> Int(processor)
                     )
     jobname = logdir * "/" * phys_model
     isdir(jobname) || mkdir(jobname)
-    jobname = @sprintf "%s/J1_%f_J2_%f_wid_01_bondD_%02i_%s" jobname J1 J2 bondD logtag
+    jobname = @sprintf "%s/J1_%f_J2_%f_wid_01_bondD_%02i_group_%i_shift_%e_%s" jobname J1 J2 bondD to_group to_shift logtag
     isdir(jobname) || mkdir(jobname)
-    jobname = @sprintf "%s/bi_%f_bf_%f" jobname 1/0.05 1/0.03 
+    jobname = @sprintf "%s/beta_%f" jobname β
 
     jobid = submitJob(env, prog, args, jobname, 
                             partitian = machine,
                             processor = processor,
                             cpu_per_task = cpu_per_task,
-                            Run = true, 
+                            Run = false,
+                            Remove=false, 
                             Wait = Wait)
 end
